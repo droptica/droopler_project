afa023d896733beccbe4bd38624276f7b4245cbb
bartek-flp <filipiuk.bartek@gmail.com>
2018-07-06 12:32:37 +0200

    Issue #17: sticky.

--- web/themes/custom/droopler_subtheme/js/global.js
+++ web/themes/custom/droopler_subtheme/js/global.js
@@ -8,19 +8,4 @@
     }
   };
 
-  Drupal.behaviors.sticky_menu = {
-    attach: function (context, settings) {
-
-      $(window).scroll(function(context) {
-        var scroll = $(window).scrollTop();
-        if (scroll >= 1) {
-          $("header.header").addClass("header-sticky");
-        } else {
-          $("header.header").removeClass("header-sticky");
-        }
-      });
-
-    }
-  };
-
 })(jQuery, Drupal);
